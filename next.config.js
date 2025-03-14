3932d8d927378057531a49ccd8d89d28ba268e9f
Saqib Amin <saqib.amin@sixlogics.com>
2025-03-14 08:06:41 +0500

    modified next.js.config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "https://apps.apple.com/app/6742452533",
-        permanent: false,
-      },
-      {
-        source: "/RootNavView/:path*",
-        destination: "https://apps.apple.com/app/6742452533",
-        permanent: false,
-      },
-    ];
-  },
-};
-
-module.exports = nextConfig;
+          async redirects() {
+            return [
+              {
+                source: "/RootNavView/:path*",
+                destination: "/[slug]?slug=:path*",
+                permanent: false,
+              },
+            ];
+          },
+        };
+        
+        module.exports = nextConfig;
+        
