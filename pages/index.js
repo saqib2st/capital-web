5e0d00400ae411a1dc64220a964036b976185ec6
Saqib Amin <saqib.amin@sixlogics.com>
2025-03-14 08:04:53 +0500

    Fix missing index.js for homepage

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,11 @@
 export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "20vh" }}>
-      <h1>Welcome to Capital Connect</h1>
-      <p>Download the app for the best experience.</p>
-      <div style={{ marginTop: "20px" }}>
-        <a
-          href="https://play.google.com/store/apps/details?id=com.capital.connect.app"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ display: "inline-block", marginRight: "20px" }}
-        >
-          <img
-            src="/android-download.png"
-            alt="Download Android"
-            width="200px"
-          />
-        </a>
-        <a
-          href="https://apps.apple.com/app/6742452533"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="/ios-download.png" alt="Download iOS" width="200px" />
-        </a>
-      </div>
-    </div>
-  );
-}
+          return (
+            <div>
+              <h1>Welcome to Capital Web</h1>
+              <p>Download our app using the links below:</p>
+              <a href="https://play.google.com/store/apps/details?id=com.capital.connect.app">Download for Android</a>
+              <a href="https://apps.apple.com/app/6742452533">Download for iOS</a>
+            </div>
+          );
+        }
+        
